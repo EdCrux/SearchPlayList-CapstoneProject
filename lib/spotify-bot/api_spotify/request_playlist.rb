cda96d336fd8ee5abc3a15a58f16607cacc7a16f
GeorginaPQ <giorginapqe@gmail.com>
2020-03-16 09:44:13 -0600

    :sparkles: Introducing the spotify module
    Introducing the spotify module to acces to the spotify API

---
+++ lib/spotify-bot/api_spotify/request_playlist.rb
@@ -0,0 +1,10 @@
+require 'json'
+require 'open-uri'
+
+module SlackSpotifybot
+    module ApiSpotify
+        class RequestPlaylist
+            
+        end
+    end
+end
--- lib/spotify-bot/commands/random_list.rb
+++ lib/spotify-bot/commands/random_list.rb
@@ -2,7 +2,13 @@ module SlackSpotifybot
     module Commands
         class RandomList < SlackRubyBot::Commands::Base
             command 'say_hi' do |client,data, _match|
-                client.say(channel: data.channel, text:'Hi! fucker!')
+                client.say(channel: data.channel, text:)
+            end
+        end
+
+        class UserList < SlackRubyBot::Commands::Base
+            command 'get_player_list' do |client,data,_match|
+                client.say()
             end
         end
     end
