79f6b6744887abe9417ab22e6c7a9742152ecdab
Eduardo <jecruxiz@gmail.com>
2020-03-17 16:03:45 -0600

    :art: Removing linter warnings and updating readme file

--- lib/spotify-bot/bot.rb
+++ lib/spotify-bot/bot.rb
@@ -13,13 +13,11 @@ module SlackSpotifybot
       command 'search_artist:' do
         title 'search_artist: <artistname>'
         desc 'Returns the artist you select'
-        long_desc 'Returns the artist related to your search. Remember to use the : then space and then the name of the artist'
       end
 
       command 'search_song:' do
         title 'search_song: <songname>'
         desc 'Returns the songs that match with your search'
-        long_desc 'Returns the posibles songs that you type. Remember to use the : then space and then the name of the song'
       end
     end
   end
--- spec/spec_search.rb
+++ spec/spec_search.rb
@@ -3,29 +3,23 @@ require './lib/spotify-bot/api_spotify/search'
 require 'yaml'
 
 VCR.configure do |config|
-    config.cassette_library_dir = File.join(File.dirname(__FILE__), 'support')
-    config.hook_into :webmock
-    config.configure_rspec_metadata!
+  config.cassette_library_dir = File.join(File.dirname(__FILE__), 'support')
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
 end
 
-
-
 describe SlackSpotifybot::ApiSpotify::Search do
-    
-  let(:artist){'dream theater'}
-  let(:song)  {'pull me under'}
-  let(:search){SlackSpotifybot::ApiSpotify::Search.new(ENV['SPOTIFY_API_TOKEN'])}
+  let(:artist) { 'dream theater' }
+  let(:song) { 'pull me under' }
+  let(:search) { SlackSpotifybot::ApiSpotify::Search.new(ENV['SPOTIFY_API_TOKEN']) }
 
   let(:user_response_artist) do
-    VCR.use_cassette('spotify2'){ search.get_http(artist,'artist') }
+    VCR.use_cassette('spotify2') { search.get_http(artist, 'artist') }
   end
 
   let(:user_response_song) do
-    VCR.use_cassette('spotify'){ search.get_http(song,'track') }
+    VCR.use_cassette('spotify') { search.get_http(song, 'track') }
   end
-
-
-
 
   describe '#transform_name' do
     it 'Returns the artist name replacing the empty spaces with %' do
@@ -35,17 +29,15 @@ describe SlackSpotifybot::ApiSpotify::Search do
       expect(search.transform_name(song)).to eql('pull%me%under')
     end
   end
-  
+
   describe '#get_http' do
-    it "Can fetch and parse song" do
-        expect(user_response_song).to be_kind_of(Hash)
-        expect(user_response_song).to have_key("tracks")
+    it 'Can fetch and parse song' do
+      expect(user_response_song).to be_kind_of(Hash)
+      expect(user_response_song).to have_key('tracks')
     end
-    it "Can fetch and parse artist" do
-        expect(user_response_artist).to be_kind_of(Hash)
-        expect(user_response_artist).to have_key("artists")
+    it 'Can fetch and parse artist' do
+      expect(user_response_artist).to be_kind_of(Hash)
+      expect(user_response_artist).to have_key('artists')
     end
   end
-
-
 end
