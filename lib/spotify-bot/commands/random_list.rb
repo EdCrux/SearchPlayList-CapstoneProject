e50b0affbda22b906c693413cf4c5af782b2f00a
Eduardo <jecruxiz@gmail.com>
2020-03-17 08:11:59 -0600

    :zap: Improving performance of the commands

--- lib/spotify-bot/commands/random_list.rb
+++ lib/spotify-bot/commands/random_list.rb
@@ -1,23 +1,21 @@
 module SlackSpotifybot
     module Commands
         class Search < SlackRubyBot::Commands::Base
-            token = 'BQD096WWBK_b-YVwvdfi9f3ink-Gn--5_umMBUUikiS_pRFV8MrYDRxoHqsgBjsxKIPR7gstqC97uS7WHgEzb_fdNYwd2_DmE0L9O_OlDquhmvHg7gHXKtfHD3mT-CC3Hbt2qwtQ3ZzoRUUmzfqeqkqBmBQl3orhRuwsTyWNdX2HbMZHu1KJ-eG0MBSlKe_OQxIpRCSUuO4p3l8zV1Udb1qgiv8yWCQBI8eJfBfYp-XPa0Fm577cHXurTUKt9-r3NxIf6f10qMuFEei0RRsWk8DafRAoe0mhGQ'
+            token = 'BQDJeG2jiPco6wihplV84EwkZXLO20IjIGjCnsP93tkcJqOh8PEJwrSBCYNCkQfdvjU7XoD-d9MwqdfZQSeAPOiqQXyYhytcNJrJcef5LgwPbWna8UI649MgOwO1Pk30RVwHjeZooUofoA6qkVu6IREduA72dBJ_Q7EMsAdNzmTOlEBnerQ9DWZ7PeSDS2KDePGhBzoY3Zp28MZ_OsHTViCcf5rl4kMWCdcUF_q4K1AAANYSKDbBW3IEcwur8S9qGQp6J9CWIJcXfSw5TZHrcxxPr2CC0ORaNw'
+            search = ApiSpotify::Search.new(token)
             
             command 'say_hi' do |client,data, _match|
                 client.say(channel: data.channel, text: "hellow")
             end
 
             command 'search_artist,' do | client, data, _match|
-
-                search = ApiSpotify::Search.new(token)
-                d = data.text.split(',')
+                d = data.text.split(':')
                 result = search.artist(d.last)
                 client.say(channel: data.channel, text: "Result of  #{result.to_s}" )
             end
 
             command 'search_song,' do |client, data, _match|
-                search_song = ApiSpotify::Search.new(token)
-                d = data.text.split(',')
+                d = data.text.split(':')
                 result = search.artist(d.last)
                 client.say(channel: data.channel, text: "Your song is #{result[0]}")
             end
